c236fd6df9e720da275c5fbc352a7f9577ddb7ad
tungduong <tungduonghmd@gmail.com>
2019-05-11 12:23:30 +0700

    add base function

---
+++ DecisionTree.py
@@ -0,0 +1,28 @@
+class Node(object):
+    def __init__(self):
+        # TO DO
+    pass
+
+
+class DecisionTree(object):
+    def __init__(self):
+        # TO DO
+        pass
+
+    def fit(self, X, y):
+        #TO DO
+        pass
+
+    def predict(self):
+        #TO DO
+        pass
+
+
+class RegressionTree(DecisionTree):
+    # TO DO
+    pass
+
+
+class ClassificationTree(DecisionTree):
+    # TO DO
+    pass
