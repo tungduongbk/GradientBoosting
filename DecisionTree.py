f279325cef57be9f860b87c8f242246f22cd23ed
tungduong <tungduonghmd@gmail.com>
2019-05-11 16:10:02 +0700

    add more attribute in baseline function

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,13 +1,17 @@
 class Node(object):
-    def __init__(self):
+    def __init__(self, data=None, left=None, right= None):
         # TO DO
-        pass
+        self.left = left
+        self.right = right
+        self.data = data
 
 
 class DecisionTree(object):
-    def __init__(self):
+    def __init__(self, max_depth=6, min_split=2):
         # TO DO
-        pass
+        self.max_depth = max_depth
+        self.min_split = min_split
+
 
     def fit(self, X, y):
         #TO DO
