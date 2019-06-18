9a3f930f9f92e775cb94bf02ba4fd1abdaece386
hoanglocbk <hoanglocbk2216@gmail.com>
2019-06-18 22:33:18 +0700

    update code

--- gbm.py
+++ gbm.py
@@ -1,21 +1,20 @@
 import numpy as np
 from DecisionTree import RegressionTree
-from data_manipulation import train_test_split, standardize, to_categorical
-from data_operation import mean_squared_error, accuracy_score
 from loss_functions import LeastSquaresError, BinomialDeviance
+
 
 class GradientBoosting(object):
     """Super class for Gradient Boosting"""
 
-     def __init__(self, loss ,n_estimators, learning_rate, min_samples_split,
+    def __init__(self, loss, n_estimators, learning_rate, min_samples_split,
                  min_impurity, max_depth, regression):
+        self.loss = loss
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
         self.min_samples_split = min_samples_split
         self.min_impurity = min_impurity
         self.max_depth = max_depth
         self.regression = regression
-        self.loss = loss
 
         # Check parameter
         if self.n_estimators <= 0:
@@ -24,29 +23,37 @@ class GradientBoosting(object):
         if self.learning_rate <= 0.0:
             raise ValueError("learning_rate must be greater than 0")
 
-        if self.loss == 'ls':
-            self.loss_func = LeastSquaresError()
+        if self.regression:
+            if self.loss == 'ls':
+                pass
         elif self.loss == 'deviance':
-            self.loss_func = BinomialDeviance()
+            pass
         else:
             raise ValueError("Loss '{0:s}' not supported. ".format(self.loss))
-
-        if not (0.0 < self.subsample <= 1.0):
-            raise ValueError("subsample must be in (0,1] but "
-                             "was %r" % self.subsample)
-
-        if not (0.0 < self.alpha < 1.0):
-            raise ValueError("alpha must be in (0.0, 1.0)")
 
     def fit(self, X, y):
         """Fit the gradient boosting model."""
 
-        self.estimators = []
+        if self.regression:
+            # regression
+            if self.loss == 'ls':
+                self.loss_func = LeastSquaresError()
+            self.loss_func.is_multi_class = False
+        else:
+            # classification
+            n_classes = np.unique(y).shape[0]
+            if n_classes == 2:
+                if self.loss == 'deviance':
+                    self.loss_func = BinomialDeviance(n_classes=1)
+            else:
+                self.loss_func.is_multi_class = True
+                self.loss_func.n_classes = n_classes
 
         # Initialize the model
         y_pred = np.full(np.shape(y), np.mean(y, axis=0))
 
         # fit the boosting stages
+        estimators = []
         for i in range(self.n_estimators):
 
             # induce regression tree on residuals
@@ -58,44 +65,53 @@ class GradientBoosting(object):
             residual = self.loss_func.negative_gradient(y, y_pred)
 
             # Fit base learner to pseudo residuals
-            tree.fit(X, residual)
+            learner = tree.fit(X, residual)
 
             # update tree leaves
-            update = tree.predict(X)
+            update = learner.predict(X)
             y_pred -= np.multiply(self.learning_rate, update)
 
             # add tree to ensemble
-            self.estimators.append(tree)
+            estimators.append(learner)
+        self.estimators = estimators
 
     def predict(self, X):
         """An estimator predicting the test data."""
+        y_pred = np.zeros(np.shape(self.estimators[0].predict(X)), dtype=np.float64)
+        for i in range(self.n_estimators):
+            y_pred =self.estimators[i].predict(X)
+            y_pred1 = np.add(y_pred,self.estimators[i].predict(X))
+            print(y_pred)
+        return y_pred
+        #return np.sum(m.predict(X) for m in self.estimators[self.n_estimators])
 
-        return np.sum(m.predict(X) for m in self.estimators)
 
-class GradientBoostingRegressor(GradientBoosting):
+class GradientBoostingRegression(GradientBoosting):
 
-    def __init__(self,loss='ls', n_estimators=200, learning_rate=0.5, min_samples_split=2,
-                 min_var_red=1e-7, max_depth=4, debug=False):
-        super(GradientBoostingRegressor, self).__init__(loss=loss,
-                                                        n_estimators=n_estimators,
-                                                        learning_rate=learning_rate,
-                                                        min_samples_split=min_samples_split,
-                                                        min_impurity=min_var_red,
-                                                        max_depth=max_depth,
-                                                        regression=True)
+    def __init__(self, loss='ls', n_estimators=200, learning_rate=0.5, min_samples_split=2,
+                 min_var_red=1e-7, max_depth=4):
+        super(GradientBoostingRegression, self).__init__(loss=loss,
+                                                         n_estimators=n_estimators,
+                                                         learning_rate=learning_rate,
+                                                         min_samples_split=min_samples_split,
+                                                         min_impurity=min_var_red,
+                                                         max_depth=max_depth,
+                                                         regression=True)
+
 
 class GradientBoostingClassifier(GradientBoosting):
 
-    def __init__(self,loss ='deviance', n_estimators=200, learning_rate=.5, min_samples_split=2,
-                 min_info_gain=1e-7, max_depth=2, debug=False):
+    def __init__(self, loss='deviance', n_estimators=200, learning_rate=.5, min_samples_split=2,
+                 min_info_gain=1e-7, max_depth=2):
         super(GradientBoostingClassifier, self).__init__(loss=loss,
-            n_estimators=n_estimators,
-            learning_rate=learning_rate,
-            min_samples_split=min_samples_split,
-            min_impurity=min_info_gain,
-            max_depth=max_depth,
-            regression=False)
+                                                         n_estimators=n_estimators,
+                                                         learning_rate=learning_rate,
+                                                         min_samples_split=min_samples_split,
+                                                         min_impurity=min_info_gain,
+                                                         max_depth=max_depth,
+                                                         regression=False)
 
-    def fit(self, X, y):
-        y = to_categorical(y)
-        super(GradientBoostingClassifier, self).fit(X, y)
+    """def fit(self, X, y):
+    #    y = to_categorical(y)
+    #    super(GradientBoostingClassifier, self).fit(X, y)
+    """
--- loss_functions.py
+++ loss_functions.py
@@ -1,14 +1,19 @@
+"""
+    Reference:
+        [1] http://statweb.stanford.edu/~tibs/book/chap14
+"""""
+
 import numpy as np
-from scipy.special import expit
-from abc import ABCMeta,abstractmethod
+from scipy.special import expit, logsumexp
+from abc import ABCMeta, abstractmethod
+
 
 class Loss(metaclass=ABCMeta):
-    is_multi_class = False
-    def __init__(self, n_classes):
-        self.K = n_classes
+    def __init__(self):
+        pass
 
     @abstractmethod
-    def __call__(self, y, pred, sample_weight=None):
+    def __call__(self, y, pred):
         """Compute the loss.
 
         Parameters
@@ -18,43 +23,60 @@ class Loss(metaclass=ABCMeta):
 
         pred : array, shape (n_samples,)
             Predicted labels
-
-        sample_weight : array-like, shape (n_samples,), optional
-            Sample weights.
+            pred is a log odds
         """
 
     @abstractmethod
-    def negative_gradient(self, y, y_pred):
+    def negative_gradient(self, y, pred):
         """Compute the negative gradient.
 
         Parameters
         ----------
-        y : array, shape (n_samples,)
+        y : array, shape (n_samples)
             The target labels.
 
-        y_pred : array, shape (n_samples,)
+        pred : array, shape (n_samples)
             The predictions.
         """
 
 
+# region Regression Losses
 class LeastSquaresError(Loss):
     """Least Squares loss function for regression"""
 
-    def __call__(self, y, y_pred, sample_weight=None):
-        if sample_weight is None:
-            return np.mean((y - y_pred.ravel()) ** 2.0)
-        else:
-            return (1.0 / sample_weight.sum() *
-                    np.sum(sample_weight * ((y - y_pred.ravel()) ** 2.0)))
+    def __call__(self, y, y_pred):
+        return np.mean((y - y_pred.ravel()) ** 2.0)
 
     def negative_gradient(self, y, y_pred):
         return y - y_pred.ravel()
-    def hessian
 
+
+class LeastAbsoluteError(Loss):
+    """Least absolute loss function for regression"""
+
+    def __call__(self, y, pred):
+        return np.sum(np.abs(y - pred))
+
+    def negative_gradient(self, y, pred):
+        return np.sign(y - pred)
+# endregion RegressionLosses
+
+
+# region Classification Losses
 class BinomialDeviance(Loss):
-    """Binomial deviance loss function for binary classification."""
+    """Binomial deviance loss function for binary classification.
+    Logistic loss function (logloss), aka binomial deviance, aka cross-entropy, aka log-likelihood loss.
 
-    def __call__(self, y, pred, sample_weight=None):
+    Parameters
+    ---------
+    pred: is a log odds
+    """
+
+    def __init__(self, n_classes):
+        self.n_classes = n_classes
+        super(Loss, self).__init__()
+
+    def __call__(self, y, pred):
         """Compute the deviance (= 2 * negative log-likelihood)"""
         pred = pred.ravel()
         return -2.0 * np.mean((y * pred) - np.logaddexp(0.0, pred))
@@ -62,3 +84,34 @@ class BinomialDeviance(Loss):
     def negative_gradient(self, y, pred):
         """Compute the residual (= negative gradient)"""
         return y - expit(pred.ravel())
+
+
+class MultinomialDeviance(Loss):
+    """Multinomial deviance loss function for multi-class classification.
+
+        Parameters
+        ----------
+        n_classes : int
+            Number of classes
+        """
+
+    def __init__(self, n_classes):
+        self.n_classes = n_classes
+        super(Loss, self).__init__()
+
+    def __call__(self, y, pred):
+        """Compute the Multinomial deviance.
+        This compute refer to equation (14.22) of Reference[1]
+        """
+
+        # create one-hot label encoding
+        Y = np.zeros((y.shape[0], self.n_classes), dtype=np.float64)
+        for k in range(self.n_classes):
+            Y[:, k] = y == k
+
+        return np.sum(-1 * (Y * pred).sum(axis=1) + logsumexp(pred, axis=1))
+
+    def negative_gradient(self, y, pred, k=0):
+        """Compute negative gradient for the ``k``-th class."""
+        return y - np.nan_to_num(np.exp(pred[:, k] - logsumexp(pred, axis=1)))
+# endregion ClassificationLosses
